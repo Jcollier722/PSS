b12ed72cc65c04b1db9195386d7289e5e522b239
unknown <Jcollier722@gmail.com>
2021-03-31 22:20:17 -0400

    fixed sjn, algorithm now works properly

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import run_simulation as sim
 
 
 if __name__ == "__main__":
+    
     job_list = read.read_spreadsheet('simulation.xlsx')
 
     fcfs = sim.fcfs(job_list)
-
     sjn = sim.sjn(job_list)
-    print(sjn[1],sjn[2])
+   
+
+
--- run_simulation.py
+++ run_simulation.py
@@ -13,9 +13,13 @@ def fcfs(job_list):
     fcfs_get_finish(this_list)
 
     #turnaround time is simply finish time - arrival time
+    #wait time is turnaround time - cycle time
     for job in this_list:
         job.turn = int(job.finish)-int(job.arrival)
         job.wait = int(job.turn) - int(job.cycle)
+
+
+    #return list and stats
     return((this_list,get_avg_turn(this_list),get_avg_wait(this_list)))
    
 
@@ -34,31 +38,58 @@ def fcfs_get_finish(job_list):
             
 #******************SJN********************#
 def sjn(job_list):
-    
+
     #take a copy
     this_list = copy.deepcopy(job_list)
+
+    #first job does not wait, first job's finish time is just its cycle
+    time_available = this_list[0].cycle
+    this_list[0].finish = time_available
+    this_list[0].wait = 0
     
-    #sort by cycle time
-    this_list = sort_by_cycle(this_list)
-    
-    
-    #sjn is fcfs sorted by cycle
-    this_list = fcfs(this_list)
-    
-    return((this_list,get_avg_turn(this_list[0]),get_avg_wait(this_list[0])))
-    
-    
-            
+    #initially every job besides the first is waiting
+    waiting_q = this_list[1:]
+
+    #while we still have processes to allocate
+    while(len(waiting_q)!=0):
+        #out of all the available jobs, pick the one with shortest cycle
+        next_job = get_next_sjn(waiting_q,time_available)
+        next_job = this_list[this_list.index(next_job)]
+        
+        #remove that job from the waiting
+        waiting_q.remove(next_job)
+
+        #set job's completion time
+        next_job.finish = int(time_available) + int(next_job.cycle)
+
+        #update the available time stamp
+        time_available = int(time_available) + int(next_job.cycle)
+
+    #set the turnaround and wait times for each job
+    for job in this_list:
+        job.turn = int(job.finish) - int(job.arrival)
+        job.wait = int(job.turn) - int(job.cycle)
+
+    return((this_list,get_avg_turn(this_list),get_avg_wait(this_list)))
+        
 """
 reference:
 https://stackoverflow.com/questions/47779513/get-an-average-value-of-a-property-from-a-list-of-objects
 """
 def get_avg_turn(job_list):
       return (sum([i.turn for i in job_list]) / len(job_list))
-    
+
 def get_avg_wait(job_list):
       return (sum([i.wait for i in job_list]) / len(job_list))
     
 def sort_by_cycle(job_list):
     return (sorted(job_list, key=lambda x: x.cycle, reverse=False))
+
+"""Get the first available job with shortest burst time"""
+def get_next_sjn(waiting,time):
+    temp_wait = sort_by_cycle(waiting)
+
+    for job in temp_wait:
+        if(job.arrival <= time):
+            return (job)
     
